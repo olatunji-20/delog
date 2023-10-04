c3a31af25579cd54034abb369c93b49cd328061e
falana sheriff <falanasheriffdeen@gmail.com>
2023-10-04 14:41:50 +0100

    worked on hero section

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="border-4 border-green-900 w-[100%] h-[1500px] grid grid-cols-1 grid-row-7 md:h-[auto] md:grid-cols-3 grid-row-2 md:px-8">
-    <div class="border-4 border-red-500 h-[820px] md:col-span-1 md:row-span-2 md:h-[auto]">
+  <div class="border-4 border-green-900 pt-8 w-[100%] h-[auto] flex flex-col flex-wrap md:h-[870px] md:flex-row md:justify-center">
+    <div class="border-4 border-red-500 w-[100%] h-[800px] md:h-[760px] md:w-[36%]">
       <div class="border-2 border-purple-400 w-[100%] h-[400px] p-2">
         <div class="border-2 border-red-500 w-[100%] h-[100%] mx-[auto]">
             <img src="/img/container.png" alt="flight-travel" class="w-[100%] h-[100%] object-cover" />
@@ -17,8 +17,43 @@
         </div>
       </div>
     </div>
-    <div class="border-2 border-blue-500 row-span-2 md:col-span-2 md:row-span-1">2</div>
-    <div class="border-2 border-yellow-500 row-span-2 md:col-span-2 md:row-span-1">3</div>
+    <div class="border-4 border-purple-400 w-[100%] h-[1208px] md:w-[60%] md:h-[760px]">
+      <div class="border-2 border-blue-500 w-[100%] h-[600px] md:h-[375px] pt-12 px-4 md:pt-4">
+        <h1 class="font-bold text-3xl">DELOG PLUS</h1>
+        <p class="pt-4 md:pt-2">Our outsource logistic department.</p>
+        <div class="border-2 border-red-400 w-[100%] h-[350px] my-8 overflow-auto md:h-[250px] md:my-4">
+          <div class="border-4 border-purple-400 w-[850px] h-[300px] flex flex-row flex-nowrap justify-between md:h-[245px] md:w-[100%]">
+            <div class="border-2 border-green-400 w-[250px] relative h-[300px] py-4 px-2 md:h-[240px] md:w-[210px]">
+              <div class="border-2 border-blue-400 w-[70px] h-[70px]"></div>
+              <h5 class="font-bold text-xl pt-4">Essential</h5>
+              <p>No money, no problem. Untitled is a next-generation financial technology.</p>
+              <div class="border-2 border-red-400 absolute py-2 bottom-0 w-[100%]">
+                <p class="">Get Started</p>
+              </div>
+            </div>
+            <div class="border-2 border-green-400 w-[250px] h-[300px] md:h-[240px] md:w-[210px]"></div>
+            <div class="border-2 border-green-400 w-[250px] h-[300px] md:h-[240px] md:w-[210px]"></div>
+          </div>
+        </div>
+        <div class="border-2 border-blue-400 w-[100%] text-center py-2 relative md:w-[120px] md:float-right md:bottom-full md:mt-4">
+          <p>Details</p>
+        </div>
+      </div>
+      <div class="border-2 border-yellow-500 w-[100%] h-[600px] md:h-[375px] pt-12 px-4 md:pt-4">
+        <h1 class="font-bold text-3xl">BLOG & NEWS</h1>
+        <p class="pt-4 md:pt-2">Best source of information for tips.</p>
+        <div class="border-2 border-red-400 w-[100%] h-[350px] my-8 overflow-auto md:h-[250px] md:my-4">
+          <div class="border-4 border-purple-400 w-[950px] h-[300px] flex flex-row flex-nowrap justify-between md:h-[245px] md:w-[100%]">
+            <div class="border-2 border-green-400 w-[280px] h-[300px] md:h-[240px] md:w-[210px]"></div>
+            <div class="border-2 border-green-400 w-[280px] h-[300px] md:h-[240px] md:w-[210px]"></div>
+            <div class="border-2 border-green-400 w-[280px] h-[300px] md:h-[240px] md:w-[210px]"></div>
+          </div>
+        </div>
+        <div class="border-2 border-blue-400 w-[100%] text-center py-2 relative md:w-[120px] md:float-right md:bottom-full md:mt-4">
+          <p>See all</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
